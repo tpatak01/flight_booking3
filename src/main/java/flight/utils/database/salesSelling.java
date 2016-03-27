d8d42f47774dd967ba663dbf7c5892a35c2aad8f
tpatak01 <pataki.tamas.83@gmail.com>
2016-03-27 16:15:32 +0100

    some authentication, possibly still bugs in it.. stared sales section and my details

---
+++ src/main/java/flight/utils/database/salesSelling.java
@@ -0,0 +1,13 @@
+/*
+ * This application is created as an Assignment project.
+ * The project required by Birkbeck University.
+ */
+package flight.utils.database;
+
+/**
+ *
+ * @author tomkabp
+ */
+public class salesSelling {
+    
+}
